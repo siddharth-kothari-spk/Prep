69e9dfbaa7954ce5d9f242ad035754f22e556aca
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-13 16:57:10 +0530

    create container, register ColorProvider and SecondVC , resolve via container on button tap

--- DI-Sample/DI-Sample/ViewController.swift
+++ DI-Sample/DI-Sample/ViewController.swift
@@ -10,13 +10,26 @@ import Swinject
 
 class ViewController: UIViewController {
 
+    let container: Container = {
+        let container = Container()
+        container.register(ColorProvider.self) { _ in
+            return ColorProviding()
+        }
+        container.register(SecondViewController.self) { resolver in
+            let vc = SecondViewController(provider: resolver.resolve(ColorProvider.self))
+            return vc
+        }
+        return container
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func tapButton(_ sender: Any) {
-        
+        guard let vc = container.resolve(SecondViewController.self) else { return }
+        present(vc, animated: true)
     }
 }
 
