f28d65a8076d90c7a8b8cfa6ac846f26c486084a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-13 16:10:28 +0530

    added secondVC and protocol ColorProvider

---
+++ DI-Sample/DI-Sample/Protocol/ColorProvider.swift
@@ -0,0 +1,14 @@
+//
+//  ColorProvider.swift
+//  DI-Sample
+//
+//  Created by Siddharth Kothari on 13/07/23.
+//
+
+import Foundation
+import UIKit
+
+
+protocol ColorProvider {
+    var color: UIColor { get }
+}
---
+++ DI-Sample/DI-Sample/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  DI-Sample
+//
+//  Created by Siddharth Kothari on 13/07/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    private let provider: ColorProvider
+    
+    init(provider: ColorProvider) {
+        self.provider = provider
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = provider.color
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
