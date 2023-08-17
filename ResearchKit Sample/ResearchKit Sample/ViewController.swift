7712e799a682ef9ad0d274c7ee324789f5730cdb
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-17 17:53:23 +0530

    ResearchKit sample

---
+++ ResearchKit Sample/ResearchKit Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResearchKit Sample
+//
+//  Created by Siddharth Kothari on 17/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
