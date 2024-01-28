52d1bd8ec2943b95cb0df388931f981e4af6ef71
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-28 16:46:13 +0530

    longestCommonPrefix: algo description

--- longestCommonPrefix.playground/Contents.swift
+++ longestCommonPrefix.playground/Contents.swift
@@ -3,6 +3,17 @@
 
      Write a function to find the longest common prefix string amongst an array of strings.
      Example: ["flower", "flow", "flight"] should return "fl".
+ 
+ Here's the breakdown of the algorithm:
+
+     The function starts by checking if the array strs is empty. If it is, an empty string is returned since there is no common prefix.
+     It then initializes the firstStr variable with the first string in the array.
+     The function uses nested loops to compare characters at the same index in each string with the corresponding character in firstStr.
+     If a character mismatch is found or if the index exceeds the length of a string, the function returns the common prefix found so far.
+     If no mismatch is found, the loop continues until all strings in the array are considered.
+     Finally, the common prefix found is returned.
+
+ This algorithm is efficient, and its time complexity is primarily determined by the length of the shortest string in the array.
  */
 
 func longestCommonPrefix(_ strs: [String]) -> String {
