1e8ca3328b1e977bfe125dbece11e5d18959af07
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-30 07:42:36 +0530

    retain cycle setup2

--- retainCycleIssue.playground/Contents.swift
+++ retainCycleIssue.playground/Contents.swift
@@ -1,3 +1,4 @@
+/*
 class A {
     var bVar: B?
     init(bVar: B? = nil) {
@@ -22,6 +23,37 @@ var classA = A()
 var classB = B()
 classA.bVar = classB
 classB.aVar = classA
-
+*/
 
 // setup 2
+class A {
+    var bVar: B?
+    init() {
+        print("Class A init called")
+    }
+    deinit {
+        print("Class A deinit called")
+    }
+}
+
+class B {
+    var aVar: A?
+    init() {
+        print("Class B init called")
+    }
+    deinit {
+        print("Class B deinit called")
+    }
+}
+
+var classA: A? = A()
+var classB: B? = B()
+
+// Creating a strong reference cycle
+classA?.bVar = classB
+classB?.aVar = classA
+print(classA as Any)
+print(classB as Any)
+// Breaking the strong reference cycle using a closure with a capture list
+classA = nil
+classB = nil
