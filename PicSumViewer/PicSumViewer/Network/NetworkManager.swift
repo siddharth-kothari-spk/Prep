117f476cb474370d26bb77177c47f7bf1d4dad2b
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-01 13:31:08 +0530

    added Network Manager to make call

--- PicSumViewer/PicSumViewer/Network/NetworkManager.swift
+++ PicSumViewer/PicSumViewer/Network/NetworkManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+class NetworkManager {
+
+    func fetchImageList(completionHandler: @escaping (Result<[ImageDetails], Error>) -> Void) {
+        guard let url = URL(string: "https://picsum.photos/list") else { return }
+
+        Task {
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let decoder = JSONDecoder()
+                let images = try decoder.decode([ImageDetails].self, from: data)
+                completionHandler(.success(images))
+                return
+            } catch let error {
+                completionHandler(.failure(error))
+                return
+            }
+        }
+    }
+}
+
--- PicSumViewer/PicSumViewer/ViewController.swift
+++ PicSumViewer/PicSumViewer/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NetworkManager().fetchImageList { result in
+                    switch result {
+                    case .success(let data):
+                        print("success data: \(data)")
+                    case .failure(let error):
+                        print("error : \(error.localizedDescription)")
+                    }
+                }
     }
-
-
 }
 
