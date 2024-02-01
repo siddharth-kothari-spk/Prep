135015ddb1e0ec7fd2bb77dd107a343081ee8767
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-01 13:35:27 +0530

    added tableView and setup constraints

--- PicSumViewer/PicSumViewer/ViewController.swift
+++ PicSumViewer/PicSumViewer/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imagesListTableView = UITableView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagesListTableView.backgroundColor = .red
+        view.addSubview(imagesListTableView)
+        setupConstraints()
+        getData()
+    }
+    
+    func setupConstraints() {
+        imagesListTableView.translatesAutoresizingMaskIntoConstraints = false
+        imagesListTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imagesListTableView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        imagesListTableView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        imagesListTableView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+    }
+    
+    func getData() {
         NetworkManager().fetchImageList { result in
                     switch result {
                     case .success(let data):
