6d93a835e41bbc55a7442fd2a97efcb62f917a99
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-03 11:16:45 +0530

    search2DMatrix: using flatmap to first flatten array and then applying contains on it

--- search2DMatrix.playground/Contents.swift
+++ search2DMatrix.playground/Contents.swift
@@ -80,3 +80,16 @@ class Solution2 {
 let sol2 = Solution2()
 print(sol2.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
 print(sol2.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+
+class Solution3 {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        
+        let flatArray = matrix.flatMap { $0 }
+        return flatArray.contains(target)
+    }
+}
+
+// Solution 3 : using flatmap to first flatten array and then applying contains on it
+let sol3 = Solution3()
+print(sol3.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
+print(sol3.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
