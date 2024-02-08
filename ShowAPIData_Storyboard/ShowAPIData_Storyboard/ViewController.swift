4128d679a788e0b936d5864a4d7a755600bb4d4e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-08 16:44:50 +0530

    ShowAPIData_Storyboard: added tableView and set constraints

--- ShowAPIData_Storyboard/ShowAPIData_Storyboard/ViewController.swift
+++ ShowAPIData_Storyboard/ShowAPIData_Storyboard/ViewController.swift
@@ -8,13 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let paginationTableView = UITableView()
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(paginationTableView)
+        paginationTableView.dataSource = self
+        paginationTableView.delegate = self
+        paginationTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        setupConstraints()
     }
 
 
+    private func setupConstraints() {
+        paginationTableView.translatesAutoresizingMaskIntoConstraints = false
+        paginationTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        paginationTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        paginationTableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        paginationTableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+    }
 }
 
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
