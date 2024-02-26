c079d9bdd8060269fdf52fa5f0aa4e52993b3a38
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-02-26 16:12:14 +0530

    kthSmallestInSortedMatrix.playground/Contents.swift

---
+++ kthSmallestInSortedMatrix.playground/Contents.swift
@@ -0,0 +1,28 @@
+/*
+ Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+
+ Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+ You must find a solution with a memory complexity better than O(n2).
+
+  
+
+ Example 1:
+
+ Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+ Output: 13
+ Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+ Example 2:
+
+ Input: matrix = [[-5]], k = 1
+ Output: -5
+ */
+
+func kthSmallest(_ matrix: [[Int]], _ k: Int) -> Int {
+    var flat = matrix.flatMap{$0}.sorted()
+    print(flat)
+    return flat[k-1]
+}
+
+print(kthSmallest([[1,5,9],[10,11,13],[12,13,15]], 8))
+print(kthSmallest([[-5]], 1))
