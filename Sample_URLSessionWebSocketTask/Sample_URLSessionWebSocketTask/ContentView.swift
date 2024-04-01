91913ed252cf7f42a2566030ed2afb98086676cf
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-01 06:56:40 +0530

    Sample_URLSessionWebSocketTask: initial commit

---
+++ Sample_URLSessionWebSocketTask/Sample_URLSessionWebSocketTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample_URLSessionWebSocketTask
+//
+//  Created by Siddharth Kothari on 01/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample_URLSessionWebSocketTask/Sample_URLSessionWebSocketTask/Sample_URLSessionWebSocketTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_URLSessionWebSocketTaskApp.swift
+//  Sample_URLSessionWebSocketTask
+//
+//  Created by Siddharth Kothari on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Sample_URLSessionWebSocketTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
