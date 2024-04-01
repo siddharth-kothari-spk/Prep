432461ab2a8d028ab9014f63c9ee43f315b4fc90
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-01 07:09:39 +0530

    Sample_URLSessionWebSocketTask: added methods to model WebSocketViewModel

--- Sample_URLSessionWebSocketTask/Sample_URLSessionWebSocketTask/WebSocketViewModel.swift
+++ Sample_URLSessionWebSocketTask/Sample_URLSessionWebSocketTask/WebSocketViewModel.swift
@@ -10,6 +10,30 @@ import SwiftUI
 import Combine
 
 class WebSocketViewModel: ObservableObject {
-    private var webSocket: URLSessionWebSocketTask?
+    private var webSocketTask: URLSessionWebSocketTask?
     @Published var message: String? = nil
+    
+    func connect() {
+            guard let url = URL(string: "wss://echo.websocket.org") else { return }
+            let session = URLSession(configuration: .default)
+            webSocketTask = session.webSocketTask(with: url)
+           // webSocketTask?.delegate = self
+            webSocketTask?.resume()
+        }
+
+        func sendMessage() {
+            message = "Hello from the client!"
+            if let message = message, let data = message.data(using: .utf8) {
+                webSocketTask?.send(.data(data)) { error in
+                    if let error = error {
+                        print("Error sending message: \(error)")
+                    }
+                }
+            }
+        }
+
+        func disconnect() {
+            webSocketTask?.cancel(with: .normalClosure, reason: nil)
+            webSocketTask = nil
+        }
 }
