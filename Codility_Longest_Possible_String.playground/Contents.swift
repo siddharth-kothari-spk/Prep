0fedab58329de44c7ceedbf0bffdbaf7c04edf52
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-15 16:10:20 +0530

    Codility_Longest_Possible_String: not working WIP

--- Codility_Longest_Possible_String.playground/Contents.swift
+++ Codility_Longest_Possible_String.playground/Contents.swift
@@ -2,44 +2,15 @@
  write a function that takes three integers as input and returns the longest possible string that can be created by joining some of the strings "AA", "AB", and "BB" together. The strings "AA", "AB", and "BB" appear a certain number of times, specified by the three input integers. However, the resulting string cannot contain the substring "AAA" or "BBB".
  */
 
-func solution(AA: Int, AB: Int, BB: Int) -> String {
-  // Initialize a string to store the result.
-  var result = ""
-
-  // Add "AB" strings as many times as possible.
-  result += String(repeating: "AB", count: min(AB, min(AA, BB)))
-
-  // Calculate the remaining occurrences of "AA" and "BB" after using them in "AB".
-  let remainingAA = AA - min(AB, min(AA, BB))
-  let remainingBB = BB - min(AB, min(AA, BB))
-
-  // Add "AA" strings as many times as possible without creating "AAA".
-  result += String(repeating: "AA", count: min(remainingAA, remainingBB))
-
-  // Add "BB" strings as many times as possible without creating "BBB".
-  result += String(repeating: "BB", count: remainingBB)
-
-  return result
-}
-
-//print(solution(AA: 5, AB: 0, BB: 2))
-
-
-func longestString(aaCount: Int, bbCount: Int, abCount: Int) -> String {
-    var aaCount = aaCount
-    var bbCount = bbCount
+func longestStringAvoidingPatterns(aCount: Int, bCount: Int, abCount: Int) -> String {
+    var aaCount = aCount
+    var bbCount = bCount
     var abCount = abCount
     var result = ""
     var lastAdded = ""
 
-    print("result: \(result), AA: \(aaCount), BB: \(bbCount), CC: \(abCount)")
     while aaCount > 0 || bbCount > 0 || abCount > 0 {
         if lastAdded == "AA" {
-//            if abCount > 0 {
-//                result += "AB"
-//                abCount -= 1
-//                lastAdded = "AB"
-//            } else if bbCount > 0 {
             if bbCount > 0 {
                 result += "BB"
                 bbCount -= 1
@@ -48,14 +19,14 @@ func longestString(aaCount: Int, bbCount: Int, abCount: Int) -> String {
                 break
             }
         } else if lastAdded == "BB" {
-            if abCount > 0 {
-                result += "AB"
-                abCount -= 1
-                lastAdded = "AB"
-            } else if aaCount > 0 {
+            if aaCount > 0 {
                 result += "AA"
                 aaCount -= 1
                 lastAdded = "AA"
+            } else if abCount > 0 {
+                result += "AB"
+                abCount -= 1
+                lastAdded = "AB"
             } else {
                 break
             }
@@ -66,28 +37,24 @@ func longestString(aaCount: Int, bbCount: Int, abCount: Int) -> String {
                     aaCount -= 1
                     lastAdded = "AA"
                 }
-            } else {
-                if bbCount > 0 {
-                    result += "BB"
-                    bbCount -= 1
-                    lastAdded = "BB"
-                }
+            } else if bbCount > 0 {
+                result += "BB"
+                bbCount -= 1
+                lastAdded = "BB"
             }
 
             // If both are zero and only AB is left
-            if aaCount == 0 && bbCount == 0 && abCount > 0 && lastAdded != "AA"{
+            if aaCount == 0 && bbCount == 0 && abCount > 0 {
                 result += "AB"
                 abCount -= 1
                 lastAdded = "AB"
             }
         }
-        print("result: \(result), AA: \(aaCount), BB: \(bbCount), AB: \(abCount), lastAdded: \(lastAdded)")
-        
     }
 
     return result
 }
 
 // Example usage
-//print(longestString(aaCount: 5, bbCount: 2, abCount: 0)) // AABBAABBAA
-print(longestString(aaCount: 1, bbCount: 1, abCount: 2))
+print(longestStringAvoidingPatterns(aCount: 1, bCount: 1, abCount: 2))
+print(longestStringAvoidingPatterns(aCount: 5, bCount: 2, abCount: 0))
