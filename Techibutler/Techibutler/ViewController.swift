15f91ff469874f24bdc3e45532de922f9046a083
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-26 19:56:31 +0530

    Techibutler: memoization implementation

--- Techibutler/Techibutler/ViewController.swift
+++ Techibutler/Techibutler/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     var posts: [Post] = []
     var currentPage = 1
     var isLoading = false
-    
+    var heightCache: [Int: CGFloat] = [:] // In actual in tableviewcell we are only showing id and title so heightCache does not make sense. It is done here just to show case how we can do memoization
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -61,6 +62,34 @@ class ViewController: UIViewController {
                 }
             }
         }
+    
+    private func heightForPost(_ post: Post) -> CGFloat {
+        // get height from cache otherwise calculate and save in cache and return
+        if let result = heightCache[post.id] {
+            return result
+        } else {
+            let result = computeHeight(post)
+            heightCache[post.id] = result
+            return result
+        }
+    }
+    
+    // Helper function to calculate cell height based on post body
+    private func computeHeight(_ post: Post) -> CGFloat {
+        let width = tableView.bounds.width - 16
+        
+        let font = UIFont.systemFont(ofSize: 16)
+        let attributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: font]
+        let estimatedSize = NSString(string: post.body).boundingRect(with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
+                                                                     options: .usesLineFragmentOrigin,
+                                                                     attributes: attributes,
+                                                                     context: nil)
+        
+        // Add padding for better readability
+        let padding: CGFloat = 16
+        
+        return estimatedSize.height + padding * 2 // Add padding for top and bottom
+    }
 }
 
 
@@ -81,8 +110,13 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
        let post = posts[indexPath.row]
        let detailViewController = DetailViewController()
        detailViewController.post = post
-        detailViewController.modalPresentationStyle = .popover
-        present(detailViewController, animated: true)
-        tableView.deselectRow(at: indexPath, animated: true)
+       detailViewController.modalPresentationStyle = .popover
+       present(detailViewController, animated: true)
+       tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+        return heightForPost(post)
     }
 }
