2496b84634c421b28fcfd836f5343336badd1ea7
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-24 12:35:19 +0530

    Sudoku: alternative solution

--- Sudoku.playground/Contents.swift
+++ Sudoku.playground/Contents.swift
@@ -85,3 +85,43 @@ sudokuBoard = [
     [".",".",".",".","8",".",".","7","9"]
 ]
 print("Valid Sudoku:", isValidSudoku2(sudokuBoard))
+
+// Alternative sol
+// https://leetcode.com/problems/sudoku-solver/solutions/1351806/solution-swift-sudoku-solver/
+
+class Solution {
+    internal typealias Char = Character
+    func solveSudoku(_ board: inout [[Char]]) -> Bool {
+        guard board.count != 0 || board[0].count != 0 else { return false }
+        return helper(&board)
+    }
+    
+    private func helper(_ board: inout [[Char]]) -> Bool {
+        for r in 0..<board.count {
+            for c in 0..<board[0].count where board[r][c] == "." {
+                for n in 1...9 where isValid(board, Char("\(n)"), r, c) {
+                    board[r][c] = Char("\(n)")
+                    if helper(&board) {
+                        return true
+                    } else {
+                        board[r][c] = "."
+                    }
+                }
+                return false
+            }
+        }
+        return true
+    }
+    
+    // Special thanks to @punk9595 for the optimization suggestion
+    private func isValid(_ board: [[Char]], _ ch: Char, _ r: Int,  _ c: Int) -> Bool {
+        for i in 0...8 {
+            if board[r][i] == ch || board[i][c] == ch { return false }
+            if board[(r / 3) * 3 + i / 3][(c / 3) * 3 + i % 3] == ch { return false }
+        }
+        return true
+    }
+}
+
+let sol = Solution()
+print(sol.solveSudoku(&sudokuBoard))
