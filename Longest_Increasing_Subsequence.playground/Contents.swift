b19dda5d141056e5a6769b56d38dc82803d21ea6
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-07-19 18:27:37 +0530

    Longest_Increasing_Subsequence.working

--- Longest_Increasing_Subsequence.playground/Contents.swift
+++ Longest_Increasing_Subsequence.playground/Contents.swift
@@ -1,3 +1,31 @@
+/*
+ Given an integer array nums, return the length of the longest strictly increasing
+ subsequence
+ .
+
+  
+
+ Example 1:
+
+ Input: nums = [10,9,2,5,3,7,101,18]
+ Output: 4
+ Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ Example 2:
+
+ Input: nums = [0,1,0,3,2,3]
+ Output: 4
+ Example 3:
+
+ Input: nums = [7,7,7,7,7,7,7]
+ Output: 1
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 2500
+ -104 <= nums[i] <= 104
+
+ */
 func lengthOfLIS(_ nums: [Int]) -> Int {
   let n = nums.count
   var dp = Array(repeating: 1, count: n) // dp array to store LIS lengths
